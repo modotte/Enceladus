26bb24de42a3273dcb20666f93d9e0f57bb6d052
hellcatto <runecroon@gmail.com>
2021-10-12 01:28:44 +0800

    Add more tests

--- Tests/UnitTest1.fs
+++ Tests/UnitTest1.fs
@@ -9,7 +9,15 @@ let Setup () =
     ()
 
 [<Test>]
-let All () =
+let RefinePathTest () =
     refinePath [|"/"; "/owo/"|] |> should equal "/owo"
     refinePath [|"/"; "/about"; "/meow"|] |> should equal "/about/meow"
     refinePath [|"/"; "/rowo"|] |> should equal "/rowo"
+
+[<Test>]
+let GetMimeTypeFromExtensionTest () =
+    getMIMETypeFromExtension "malkin.html" |> should equal (".html", "text/html")
+    getMIMETypeFromExtension "M723Jhhs.jpg" |> should equal (".jpg", "text/plain")
+    getMIMETypeFromExtension "meow.gmi" |> should equal (".gmi", "text/gemini")
+    getMIMETypeFromExtension "82yqoiAWJKS.txt" |> should equal (".txt", "text/plain")
+    getMIMETypeFromExtension "megacol" |> should equal ("", "text/plain")
