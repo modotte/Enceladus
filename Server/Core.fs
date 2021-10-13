51d993029feae21baad370801175975040d4f968
hellcatto <runecroon@gmail.com>
2021-10-13 17:57:28 +0800

    Modularize

---
+++ Server/Core.fs
@@ -0,0 +1,39 @@
+namespace Enceladus
+
+open System
+open System.IO
+
+module Core =
+    type StatusCode =
+        | Input
+        | Success
+        | Redirect
+        | TemporaryFailure
+        | PermanentFailure
+        | ClientCertificateRequired
+        
+    let getStatusCode = function
+    | Input -> 10
+    | Success -> 20
+    | Redirect -> 30
+    | TemporaryFailure -> 40
+    | PermanentFailure -> 50
+    | ClientCertificateRequired -> 60
+    
+    let getMIMETypeFromExtension (filename: string) =
+        let extension = Path.GetExtension(filename)
+        match extension with
+        | ".html" | ".xhtml" | ".htm" | ".xhtm" -> (extension, "text/html")
+        | ".md" -> (extension, "text/markdown")
+        | ".gmi" | ".gemini" -> (extension, "text/gemini")
+        | _ -> (extension, "text/plain")
+
+    let refinePath (pathSegments: string array) =
+        let removeTrailingSlash (str: string) =
+            str.TrimEnd([|'/'|])
+
+        match Array.length pathSegments with
+        | 2 -> Array.last pathSegments |> removeTrailingSlash
+        | _ -> 
+            let path = Array.skip 0 pathSegments |> String.Concat
+            path.[1..] |> removeTrailingSlash
--- Server/Server.fs
+++ Server/Server.fs
@@ -11,16 +11,10 @@ open System.IO
 open System.Text
 open Serilog
 
+open Enceladus.Core
+
 
 module Server =
-    type StatusCode =
-        | Input
-        | Success
-        | Redirect
-        | TemporaryFailure
-        | PermanentFailure
-        | ClientCertificateRequired
-
     type ClientHandlingResult =
         | Success of int * string
         | FileDoesntExistError of string
@@ -34,22 +28,6 @@ module Server =
         port: int
         staticDirectory: string
     }
-
-    let getStatusCode = function
-        | Input -> 10
-        | StatusCode.Success -> 20
-        | Redirect -> 30
-        | TemporaryFailure -> 40
-        | PermanentFailure -> 50
-        | ClientCertificateRequired -> 60
-        
-    let getMIMETypeFromExtension (filename: string) =
-        let extension = Path.GetExtension(filename)
-        match extension with
-        | ".html" | ".xhtml" | ".htm" | ".xhtm" -> (extension, "text/html")
-        | ".md" -> (extension, "text/markdown")
-        | ".gmi" | ".gemini" -> (extension, "text/gemini")
-        | _ -> (extension, "text/plain")
         
     let getFile (filename: string) (directory: string) =
         try
@@ -58,16 +36,6 @@ module Server =
         with
         | :? DirectoryNotFoundException as exn ->
             Error exn
-            
-    let refinePath (pathSegments: string array) =
-        let removeTrailingSlash (str: string) =
-            str.TrimEnd([|'/'|])
-
-        match Array.length pathSegments with
-        | 2 -> Array.last pathSegments |> removeTrailingSlash
-        | _ -> 
-            let path = Array.skip 0 pathSegments |> String.Concat
-            path.[1..] |> removeTrailingSlash
             
     let writeHeaderResponse (sslStream: SslStream) (statusCode: StatusCode) (mime: string option) (errorMessage: string option) =
         match statusCode with
