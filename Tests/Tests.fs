0bc97a89396c5dfb17c1fb2d82509592e96b26b7
hellcatto <runecroon@gmail.com>
2021-10-14 22:13:59 +0800

    Update tests

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -4,22 +4,20 @@ open NUnit.Framework
 open FsUnit
 open Enceladus.Core
 
-[<SetUp>]
-let Setup () =
-    ()
+[<Test>]
+let GetMimeTypeFromExtensionTest () =
+    getMIMETypeFromExtension "malkin.html" |> should equal "text/html"
+    getMIMETypeFromExtension "M723Jhhs.jpg" |> should equal "image/jpeg"
+    getMIMETypeFromExtension "meow.gmi" |> should equal "text/gemini"
+    getMIMETypeFromExtension "82yqoiAWJKS.txt" |> should equal "text/plain"
+    getMIMETypeFromExtension "megacol" |> should equal "text/plain"
+    getMIMETypeFromExtension "jhawhsha.jpeg" |> should equal "image/jpeg"
+    getMIMETypeFromExtension "8wjs0002j020hahsiowa.png" |> should equal "image/png"    
 
 [<Test>]
 let RefinePathTest () =
-    refinePath [|"/"; "/owo/"|] |> should equal "/owo"
-    refinePath [|"/"; "/about"; "/meow"|] |> should equal "/about/meow"
-    refinePath [|"/"; "/rowo"|] |> should equal "/rowo"
+    refinePath [|"/"; "/owo/"|] |> should equal ("", "owo")
+    refinePath [|"/"; "/about"; "/meow"|] |> should equal ("about", "meow")
+    refinePath [|"/"; "/rowo"|] |> should equal ("", "rowo")
+    refinePath [|"/"; "/subdir"; "/code"; "c"|] |> should equal ("subdir/code", "c")
 
-[<Test>]
-let GetMimeTypeFromExtensionTest () =
-    getMIMETypeFromExtension "malkin.html" |> should equal (".html", "text/html")
-    getMIMETypeFromExtension "M723Jhhs.jpg" |> should equal (".jpg", "image/jpeg")
-    getMIMETypeFromExtension "meow.gmi" |> should equal (".gmi", "text/gemini")
-    getMIMETypeFromExtension "82yqoiAWJKS.txt" |> should equal (".txt", "text/plain")
-    getMIMETypeFromExtension "megacol" |> should equal ("", "text/plain")
-    getMIMETypeFromExtension "jhawhsha.jpeg" |> should equal (".jpeg", "image/jpeg")
-    getMIMETypeFromExtension "8wjs0002j020hahsiowa.png" |> should equal (".png", "image/png")
